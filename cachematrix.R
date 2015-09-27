a8e02d92aa7914f4325b663bada2bd2b3bf0d0ff
John Cashe <SimplyJohn@users.noreply.github.com>
2015-09-27 21:01:10 +0300

    Update cachematrix.R
    both functions written like special vector was

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverseMat <- NULL
+  set <- function(y) {
+    x <<- y
+    inverseMat <<- NULL
+  }
+  get <- function() x
+  setInverseMat <- function(im) inverseMat <<- im
+  getInverseMat <- function() inverseMat
+  list(set = set, get = get,
+       setInverseMat = setInverseMat,
+       getInverseMat = getInverseMat)  
 }
 
 
-## Write a short comment describing this function
+## This functions finds the inverse matrix of a "special" cached matrix.
+## if the inverse was already calculated it retrieves it from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  im <- x$getInverseMat()
+  data <- x$get()
+  
+  if(!is.null(im)) {
+    message("getting inverse cached matrix")
+    return(im)
+  }
+  
+  im <- solve(data, ...)
+  x$setInverseMat(im)
+  im
 }
